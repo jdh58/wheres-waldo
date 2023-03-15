e5f61b46ff5a048ea91f6c43c047679c0f4fb90c
Jonathan Hawes <jhawes580@gmail.com>
2023-03-14 21:39:41 -0700

    Mostly style the game page

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,16 @@
 import Header from './Header';
 import './styles/Game.css';
+import GameImage from './assets/zyro-image.png';
 
 export default function Game(props) {
   return (
     <>
       <Header />
       <div className="gamePage">
-        <img src="" alt="" className="gameImage" />
+        <h1 className="character-banner">Willy Wonka</h1>
+        <div className="gameImage">
+          <img src={GameImage} alt="" className="gameImage" />
+        </div>
         <h2 className="timer">00:15:76</h2>
       </div>
     </>
